a1933c18a14c3c06073896a72bcb197afb5efa0c
Gurveer Madurai <maduraigurveer@gmail.com>
2023-03-28 16:20:52 -0700

    changes to JS

--- script.js
+++ script.js
@@ -15,6 +15,71 @@ var question = [
 
 
 var current = 0; 
+var points = 0;
 
 var questionEl = document.querySelector('#question-content');
-var optionsEl = document.querySelector('#answer1','#answer2','#answer3','#answer4')
+var optionsEl = document.querySelector('#answer1','#answer2','#answer3','#answer4');
+
+var chooseOption = function(){
+
+var correctOptionIndex = data[current].answer;
+
+    current++;
+    optionsEl.innerHTML = '';
+    putQuestion(current);
+    putOptions(current);
+};
+
+var putQuestion = function (index){
+    questionEl.textContent = data[index].question;
+}
+
+var putOptions = function (index){
+    var options = data[index].options;
+    for (var i = 0; i < options.length; i++){
+        var options = options[i];
+        var buttonEl = document.createElement('button');
+        buttonEl.textContent = options;
+        optionsEl.appendChild(buttonEl);
+    }
+}
+
+//above code not rendering onto page ? debug
+
+// timer declaration
+var timerContainerElem = document.getElementById("timer-container");
+var timerEl = document.getElementById("timer");
+var timeLeft = 60;
+var endGame = false;
+var currentScore = 0;
+var highscore = JSON.parse(localStorage.getItem('highscores')) || {};
+
+// create the timer
+
+var startTime = () => {
+    timeLeft = 60;
+    var timeDecrease = setInterval(() => {
+        timeLeft--;
+        timerEl.textContent = timeLeft;
+        if(endGame === true){
+            clearInterval(timeDecrease);
+        }
+        else(timeLeft < 0) {
+            clearInterval(timeDecrease);
+            alert('You are out of time! Your score is: '+currentScore);
+            return;
+        }
+    }, 6000); //60 seconds ?
+}
+
+
+//removing 15 seconds on timer
+
+var subtractTime = () => {
+    if (timeLeft > 15) {
+        timeLeft -= 15;
+    } else {
+        timeLeft = 0;
+    }
+}
+
